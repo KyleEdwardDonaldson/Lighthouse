e9bd0b799e8068710da42382af7ec616149f62cf
Kyle <kyle@ked.dev>
2025-09-08 19:11:33 +0100

    Configure custom domain lighthouse.ked.dev

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://KyleEdwardDonaldson.github.io',
+  url: 'https://lighthouse.ked.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Lighthouse/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
